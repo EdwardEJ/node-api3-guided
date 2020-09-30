d0097b51a1ea30628dae0704b41ee9ab7252aa93
A <jesus.e.estrada@hotmail.com>
2020-09-30 14:14:52 -0500

    update

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 const express = require('express'); // importing a CommonJS module
-
+const morgan = require('morgan');
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
+const logger = morgan('combined');
+
+// global middleware
+function greeter(req, res, next) {
+  console.log('hello');
+
+  req.name = 'sam';
+
+  next();
+}
+
+function passwordValid(req, res, next) {
+  if (req.headers.password === 'melon') {
+    next();
+  } else {
+    res.status(401).json({ errorMessage: 'Incorrect password' });
+  }
+}
 
 server.use(express.json());
-
-server.use('/api/hubs', hubsRouter);
+server.use(logger);
+server.use(greeter);
+// server.use(passwordValid);
 
 server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+  const password = req.headers.password;
+  const nameInsert = req.name ? ` ${req.name}` : '';
 
-  res.send(`
-    <h2>Lambda Hubs API</h2>
-    <p>Welcome${nameInsert} to the Lambda Hubs API</p>
-    `);
+  res.status(200).json({ name: nameInsert, password });
 });
+
+server.use('/api/hubs', passwordValid, hubsRouter);
 
 module.exports = server;
